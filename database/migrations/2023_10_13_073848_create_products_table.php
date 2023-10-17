d901fefaee364416e8f1cbdef9cbe99e0d740c93
Roma <129154115+wascrashed@users.noreply.github.com>
2023-10-17 21:06:53 +0300

    Update 2023_10_13_073848_create_products_table.php

--- database/migrations/2023_10_13_073848_create_products_table.php
+++ database/migrations/2023_10_13_073848_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->float('price');
+            $table->integer('price');
             $table->timestamps();
         });
     }
